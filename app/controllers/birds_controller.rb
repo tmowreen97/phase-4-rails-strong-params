4ba0005b61cdc3149db2095fbef590f20a3e86e0
Tasreen Mowreen <tmowreen97@gmail.com>
2023-01-25 14:05:37 -0500

    commit tree a94114fd693d56e46bb597359dfb7d6d3d1511f0

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,12 +6,6 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -20,6 +14,19 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  # POST /birds
+  def create
+    bird = Bird.create(bird_params)
+    render json: bird, status: :created
+  end
+
+  private
+  # all methods below here are private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
